cbc1a26006eddbc168987bee70bae0daa4b435ad
samuelalvarez034 <you@example.com>
2025-03-27 10:02:46 +0100

    PIOT-GDA-07-001

--- src/main/java/programmingtheiot/gda/connection/MqttClientConnector.java
+++ src/main/java/programmingtheiot/gda/connection/MqttClientConnector.java
@@ -37,6 +37,20 @@ public class MqttClientConnector implements IPubSubClient, MqttCallbackExtended
 	
 	private static final Logger _Logger =
 		Logger.getLogger(MqttClientConnector.class.getName());
+
+	private boolean useAsyncClient = false;
+
+	private MqttClient           mqttClient = null;
+	private MqttConnectOptions   connOpts = null;
+	private MemoryPersistence    persistence = null;
+	private IDataMessageListener dataMsgListener = null;
+	
+	private String  clientID = null;
+	private String  brokerAddr = null;
+	private String  host = ConfigConst.DEFAULT_HOST;
+	private String  protocol = ConfigConst.DEFAULT_MQTT_PROTOCOL;
+	private int     port = ConfigConst.DEFAULT_MQTT_PORT;
+	private int     brokerKeepAlive = ConfigConst.DEFAULT_KEEP_ALIVE;
 	
 	// params
 	
@@ -50,6 +64,61 @@ public class MqttClientConnector implements IPubSubClient, MqttCallbackExtended
 	public MqttClientConnector()
 	{
 		super();
+
+		ConfigUtil configUtil = ConfigUtil.getInstance();
+
+		this.host =
+			configUtil.getProperty(
+				ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.HOST_KEY, ConfigConst.DEFAULT_HOST);
+
+		this.port =
+			configUtil.getInteger(
+				ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.PORT_KEY, ConfigConst.DEFAULT_MQTT_PORT);
+
+		this.brokerKeepAlive =
+			configUtil.getInteger(
+				ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.KEEP_ALIVE_KEY, ConfigConst.DEFAULT_KEEP_ALIVE);
+
+		// This next config file boolean property is optional; it can be
+		// set within the [Mqtt.GatewayService] and [Cloud.GatewayService]
+		// sections of PiotConfig.props. You can use it to create a logical
+		// flow within this class to determine whether to use MqttClient
+		// or MqttAsyncClient, or simply choose one of the two classes based
+		// on your usage needs. Generally speaking, MqttAsyncClient will
+		// be necessary when running the GDA as an application, as it will
+		// need to handle incoming and outgoing messages using MQTT
+		// simultaneously. For GDA-only testing using the test cases
+		// specified in this lab module and others, it's generally best -
+		// and likely required - to use MqttClient.
+		//
+		// IMPORTANT: If you're using an older version of ConfigConst.java,
+		// you'll need to add the following line of code to ConfigConst.java:
+		// public static final String USE_ASYNC_CLIENT_KEY = "useAsyncClient";
+		this.useAsyncClient =
+			configUtil.getBoolean(
+				ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.USE_ASYNC_CLIENT_KEY);
+
+		// NOTE: paho Java client requires a client ID - for now, you
+		// can use the generated client ID; for later exercises, you
+		// should define your own and load it from the config file
+		this.clientID = MqttClient.generateClientId();
+
+		// these are specific to the MQTT connection which will be used during connect
+		this.persistence = new MemoryPersistence();
+		this.connOpts = new MqttConnectOptions();
+
+		this.connOpts.setKeepAliveInterval(this.brokerKeepAlive);
+
+		// NOTE: If using a random clientID for each new connection,
+		// clean session should be 'true'; see MQTT spec for details
+		this.connOpts.setCleanSession(false);
+
+		// NOTE: Auto-reconnect can be a useful connection recovery feature
+		this.connOpts.setAutomaticReconnect(true);
+
+		// NOTE: URL does not have a protocol handler for "tcp",
+		// so we need to construct the URL manually
+		this.brokerAddr = this.protocol + "://" + this.host + ":" + this.port;
 	}
 	
 	
@@ -58,12 +127,45 @@ public class MqttClientConnector implements IPubSubClient, MqttCallbackExtended
 	@Override
 	public boolean connectClient()
 	{
+		try {
+			if (this.mqttClient == null) {
+				this.mqttClient = new MqttClient(this.brokerAddr, this.clientID, this.persistence);
+				this.mqttClient.setCallback(this);
+			}
+	
+			if (! this.mqttClient.isConnected()) {
+				_Logger.info("MQTT client connecting to broker: " + this.brokerAddr);
+				this.mqttClient.connect(this.connOpts);
+				return true;
+			} else {
+				_Logger.warning("MQTT client already connected to broker: " + this.brokerAddr);
+			}
+		} catch (MqttException e) {
+			// TODO: handle this exception
+			_Logger.log(Level.SEVERE, "Failed to connect MQTT client to broker.", e);
+		}
+	
 		return false;
 	}
 
 	@Override
 	public boolean disconnectClient()
 	{
+		try {
+			if (this.mqttClient != null) {
+				if (this.mqttClient.isConnected()) {
+					_Logger.info("Disconnecting MQTT client from broker: " + this.brokerAddr);
+					this.mqttClient.disconnect();
+					return true;
+				} else {
+					_Logger.warning("MQTT client not connected to broker: " + this.brokerAddr);
+				}
+			}
+		} catch (Exception e) {
+			// TODO: handle this exception
+			_Logger.log(Level.SEVERE, "Failed to disconnect MQTT client from broker: " + this.brokerAddr, e);
+		}
+	
 		return false;
 	}
 
@@ -75,12 +177,14 @@ public class MqttClientConnector implements IPubSubClient, MqttCallbackExtended
 	@Override
 	public boolean publishMessage(ResourceNameEnum topicName, String msg, int qos)
 	{
+		_Logger.info("publishMessage was called");
 		return false;
 	}
 
 	@Override
 	public boolean subscribeToTopic(ResourceNameEnum topicName, int qos)
 	{
+		_Logger.info("subscribeToTopic was called");
 		return false;
 	}
 
@@ -99,6 +203,11 @@ public class MqttClientConnector implements IPubSubClient, MqttCallbackExtended
 	@Override
 	public boolean setDataMessageListener(IDataMessageListener listener)
 	{
+		if (listener != null) {
+			this.dataMsgListener = listener;
+			return true;
+		}
+	
 		return false;
 	}
 	

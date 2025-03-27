24acf1d7f0c8ebd331d2856456a85a5995d74b7a
samuelalvarez034 <you@example.com>
2025-03-27 10:11:00 +0100

    PIOT-GDA-07-002

--- src/main/java/programmingtheiot/gda/connection/MqttClientConnector.java
+++ src/main/java/programmingtheiot/gda/connection/MqttClientConnector.java
@@ -216,21 +216,27 @@ public class MqttClientConnector implements IPubSubClient, MqttCallbackExtended
 	@Override
 	public void connectComplete(boolean reconnect, String serverURI)
 	{
+		_Logger.info("MQTT connection successful (is reconnect = " + reconnect + "). Broker: " + serverURI);
 	}
 
 	@Override
 	public void connectionLost(Throwable t)
 	{
+		_Logger.log(Level.WARNING, "Lost connection to MQTT broker: " + this.brokerAddr, t);
+
 	}
 	
 	@Override
 	public void deliveryComplete(IMqttDeliveryToken token)
 	{
+		_Logger.fine("Delivered MQTT message with ID: " + token.getMessageId());
 	}
 	
 	@Override
 	public void messageArrived(String topic, MqttMessage msg) throws Exception
 	{
+		_Logger.info("MQTT message arrived on topic: '" + topic + "'");
+
 	}
 
 	
